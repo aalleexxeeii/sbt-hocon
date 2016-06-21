3a161c18251b164f7e19961ca851b57c686e12f6
aalleexxeeii <aalleexxeeii@github.com>
2016-06-21 21:25:01 +0300

    hoconDefaults task

--- project/HoconPlugin.scala
+++ project/HoconPlugin.scala
@@ -5,7 +5,6 @@ import java.io.{FileOutputStream, PrintWriter}
 import com.typesafe.config._
 import sbt.Keys._
 import sbt.classpath.ClasspathUtilities
-import sbt.std.TaskStreams
 import sbt.{AutoPlugin, _}
 
 import scala.collection.JavaConverters._
@@ -16,6 +15,7 @@ object HoconPlugin extends AutoPlugin {
   object autoImport {
     lazy val hoconExtraResources = settingKey[Seq[String]]("additional reference files to consider")
     lazy val hoconPurify = inputKey[String]("Purify source HOCON configuration removing settings coniciding with defaults")
+    lazy val hoconDefaults = inputKey[String]("Generate joint HOCON configuration with defaults")
 
     lazy val basePurifySettings: Seq[Def.Setting[_]] = Seq(
       hoconExtraResources := Nil,
@@ -24,52 +24,56 @@ object HoconPlugin extends AutoPlugin {
         val args = Def.spaceDelimited("<input> <output>").parsed
         val (i, o) = args match {
           case Seq(i, o) ⇒ (i, o)
-          case _ ⇒ sys.error(s"Use: purify <input> <output>")
+          case _ ⇒ sys.error(s"Use: hoconPurify <input> <output>")
         }
         val taskStreams = streams.value
-        val cp: Seq[Attributed[File]] = (fullClasspath in Compile).value
-        val loader = ClasspathUtilities.toLoader(cp map (_.data))
-        val inputSource = i match {
-          case "-" ⇒ Source.fromInputStream(System.in)
-          case path ⇒ Source.fromFile(path)
-        }
-        val outputWriter = o match {
-          case "-" ⇒
-            //streams.value.text()
-            new PrintWriter(System.out)
-          case path ⇒ new PrintWriter(new FileOutputStream(path))
-        }
 
-        val input = inputSource.getLines() mkString "\n"
-        apply(loader = loader, output = outputWriter, input = input, streams = taskStreams, extraResources = hoconExtraResources.value)
+        purify(
+          loader = createLoader((fullClasspath in Compile).value),
+          input = readInput(i),
+          output = outputWriter(o),
+          extraResources = hoconExtraResources.value
+        )
+      },
+      hoconDefaults := {
+        val args = Def.spaceDelimited("[<output>]").parsed
+        if (args.size > 1) sys.error(s"Use: hoconDefaults [<output>]")
+        defaults(
+          loader = createLoader((fullClasspath in Compile).value),
+          output = outputWriter(args.headOption getOrElse "-"),
+          extraResources = hoconExtraResources.value
+        )
       }
     )
   }
 
   import autoImport._
 
+  def createLoader(cp: Seq[Attributed[File]]) = ClasspathUtilities.toLoader(cp map (_.data))
+
   override val projectSettings = basePurifySettings
   //inConfig(Compile)(basePurifySettings)
 
-  def apply(loader: ClassLoader, input: String, output: PrintWriter, extraResources: Seq[String] = Nil, streams: TaskStreams[_]) = {
-    val logger = streams.log
-    val inputConfig = ConfigFactory.parseString(input, parseOptions)
-    def parse(path: String) =
-      ConfigFactory.parseResources(loader, path, parseOptions)
-    val referenceConfig = parse("reference.conf")
+  def readDefaults(loader: ClassLoader, extraResources: Seq[String] = Nil) = {
+    def parseResource(path: String) = ConfigFactory.parseResources(loader, path, parseOptions)
+    val referenceConfig = parseResource("reference.conf")
+    extraResources.foldRight(referenceConfig)(parseResource(_) withFallback _)
+  }
 
-    val completeConfig = extraResources.foldRight(referenceConfig)(parse(_) withFallback _)
+  def purify(loader: ClassLoader, input: String, output: PrintWriter, extraResources: Seq[String] = Nil) = {
+    val inputConfig = ConfigFactory.parseString(input, parseOptions)
+    val defaults = readDefaults(loader, extraResources)
 
     val inputSet = toPairSet(inputConfig)
-    val referenceSet = toPairSet(completeConfig)
-    val referenceMap = referenceSet.toMap
+    val defaultsSet = toPairSet(defaults)
+    val defaultsMap = defaultsSet.toMap
 
-    var diff = inputSet.diff(referenceSet)
+    var diff = inputSet.diff(defaultsSet)
 
     if (true /* TODO: setting for cloning comments */ ) {
       diff = diff map {
         case (key, value) ⇒
-          key → referenceMap.get(key).map { v ⇒
+          key → defaultsMap.get(key).map { v ⇒
             val referenceComments = comments(v)
             val inputComments = comments(value)
             value.withOrigin(
@@ -82,15 +86,39 @@ object HoconPlugin extends AutoPlugin {
     }
 
     val restored = ConfigFactory.parseMap(diff.toMap.asJava)
-    val rendered = render(restored)
+    dump(restored, output)
+  }
+
+  def defaults(loader: ClassLoader, output: PrintWriter, extraResources: Seq[String] = Nil) = {
+    val config = readDefaults(loader, extraResources)
+    dump(config, output)
+  }
+
+  def dump(config: Config, output: PrintWriter): String = {
+    val rendered = render(config)
     output.print(rendered)
+    output.flush()
     output.close()
     rendered
   }
 
-
   protected val resolveOptions = ConfigResolveOptions.defaults().setAllowUnresolved(true).setUseSystemEnvironment(false)
   protected val parseOptions = ConfigParseOptions.defaults().setAllowMissing(true)
+
+  protected def readInput(path: String): String = {
+    path match {
+      case "-" ⇒ Source.fromInputStream(System.in)
+      case _ ⇒ Source.fromFile(path)
+    }
+  }.getLines() mkString "\n"
+
+  def outputWriter(path: String) = path match {
+    case "-" ⇒
+      //streams.value.text()
+      new PrintWriter(System.out)
+    case _ ⇒ new PrintWriter(new FileOutputStream(path))
+  }
+
 
   def toPairSet(config: Config) =
     config.resolve(resolveOptions).entrySet().asScala.map(e ⇒ e.getKey → e.getValue)
